11d68c2961626164ecf606e85e6ddba7ef5c46cc
younjungpark <geoffrey.park@altibase.com>
2022-01-12 17:57:49 +0900

    SampleToAltibase 에서 불필요하게 schema를 생성하는 부분 제거

--- SiardCmd/test/ch/admin/bar/siard2/cmd/AltibaseToDbTester.java
+++ SiardCmd/test/ch/admin/bar/siard2/cmd/AltibaseToDbTester.java
@@ -1,16 +1,12 @@
 package ch.admin.bar.siard2.cmd;
 
-import ch.admin.bar.siard2.jdbc.AltibaseConnection;
 import ch.admin.bar.siard2.jdbc.AltibaseDriver;
-import ch.admin.bar.siard2.jdbcx.AltibaseDataSource;
 import ch.enterag.utils.EU;
 import ch.enterag.utils.base.ConnectionProperties;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -63,40 +59,6 @@ public class AltibaseToDbTester extends BaseFromDbTester
         System.out.println("testSampleToAltibase");
         try
         {
-            AltibaseDataSource dsAltibase = new AltibaseDataSource();
-            dsAltibase.setUrl(_sALTIBASE_DB_URL);
-            dsAltibase.setUser(_sALTIBASE_DBA_USER);
-            dsAltibase.setPassword(_sALTIBASE_DBA_PASSWORD);
-            AltibaseConnection connAltibase = (AltibaseConnection)dsAltibase.getConnection();
-            connAltibase.setAutoCommit(false);
-            try
-            {
-                Statement stmt = connAltibase.createStatement();
-                stmt.executeUpdate("DROP SCHEMA " + _sALTIBASE_TEST_SCHEMA + " CASCADE");
-                stmt.close();
-                connAltibase.commit();
-            }
-            catch(SQLException se)
-            {
-                System.out.println(EU.getExceptionMessage(se));
-                /* terminate transaction */
-                try { connAltibase.rollback(); }
-                catch(SQLException seRollback) { System.out.println("Rollback failed with "+EU.getExceptionMessage(seRollback)); }
-            }
-            Statement stmt = connAltibase.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            String sSql = "CREATE SCHEMA " + _sALTIBASE_TEST_SCHEMA + " AUTHORIZATION " + _sALTIBASE_DB_USER;
-            int iResult = stmt.executeUpdate(sSql);
-            if (iResult == 0)
-                connAltibase.commit();
-            else
-            {
-                connAltibase.rollback();
-                fail(sSql + " failed!");
-            }
-            sSql = "GRANT ALL PRIVILEGES TO " + _sALTIBASE_TEST_SCHEMA;
-           stmt.unwrap(Statement.class).executeUpdate(sSql);
-            stmt.close();
-            connAltibase.close();
             /* now upload sample */
             String[] args = new String[]{
                     "-o",
